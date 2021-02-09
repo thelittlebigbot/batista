f3f51c3a6c6ee7a46461ebc989ca5de7b5547604
Alexandre Figueiredo <alexandre@blackfoot.io>
2021-02-09 19:12:19 +0100

    feat: create the entire app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import logo from './logo.png';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const api =
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+
+  const getAndSetData = async () => {
+    setLoading(true);
+    await axios.get(api).then((res) => {
+      const tmp = res.data;
+
+      tmp.map((i) => {
+        if (i.symbol === 'bat') {
+          return setData(i);
+        } else return null;
+      });
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getAndSetData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className='app'>
+        <img className='loading' src={logo} alt='Loading' />
+      </div>
+    );
+  } else {
+    return (
+      <div className='app'>
+        <img className='icon' src={data.image} alt='Icon' />
+        <span className='price'>{data.current_price}</span>
+      </div>
+    );
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('app'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
